d5caade91ab2b50491c58e9a22bd67e872e34c13
amit-i <amit.irodha@quovantis.com>
2016-05-10 10:56:54 +0530

    intial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(firstgitproject.Startup))]
+namespace firstgitproject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
