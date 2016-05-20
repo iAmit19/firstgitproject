2a60fbd22201cb41abf94aadbdb776a73e12cdb4
amit-i <amit.irodha@quovantis.com>
2016-05-20 10:48:53 +0530

    jenkins session

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace firstgitproject.Controllers
         {
             return View();
         }
-
+//test
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
--- Models/ManageViewModels.cs
+++ Models/ManageViewModels.cs
@@ -12,6 +12,7 @@ namespace firstgitproject.Models
         public string PhoneNumber { get; set; }
         public bool TwoFactor { get; set; }
         public bool BrowserRemembered { get; set; }
+		public bool BrowserRemembered1 { get; set; }
     }
 
     public class ManageLoginsViewModel
